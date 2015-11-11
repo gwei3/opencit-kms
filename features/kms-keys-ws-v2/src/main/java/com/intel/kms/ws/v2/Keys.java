3b355f90d172ecbbdafa314334c58cf07b974109
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2015-11-11 03:22:22 -0800

    dev-cit-3.0-sprint3: fix bug #4780 transfer key API gives 401 unauthorized error for valid user

--- features/kms-keys-ws-v2/src/main/java/com/intel/kms/ws/v2/Keys.java
+++ features/kms-keys-ws-v2/src/main/java/com/intel/kms/ws/v2/Keys.java
@@ -15,6 +15,7 @@ import com.intel.mtwilson.jaxrs2.NoLinks;
 import com.intel.mtwilson.jaxrs2.mediatype.CryptoMediaType;
 import com.intel.mtwilson.jaxrs2.server.resource.AbstractJsonapiResource;
 import com.intel.mtwilson.launcher.ws.ext.V2;
+import com.intel.mtwilson.shiro.ShiroUtil;
 import com.intel.mtwilson.shiro.Username;
 import com.intel.mtwilson.util.validation.faults.Thrown;
 import com.thoughtworks.xstream.XStream;
@@ -104,7 +105,7 @@ public class Keys extends AbstractJsonapiResource<Key, KeyCollection, KeyFilterC
         log.debug("transferKey");
         TransferKeyRequest keyRequest = new TransferKeyRequest();
         keyRequest.setKeyId(keyId);
-        keyRequest.setUsername(getLoginUsername(httpServletRequest));
+        keyRequest.setUsername(ShiroUtil.subjectUsername());
         try {
         return getRepository().getKeyManager().transferKey(keyRequest);
         }
@@ -124,59 +125,11 @@ public class Keys extends AbstractJsonapiResource<Key, KeyCollection, KeyFilterC
         log.debug("transferKeyPEM");
         TransferKeyRequest transferKeyRequest = new TransferKeyRequest();
         transferKeyRequest.setKeyId(keyId);
-        transferKeyRequest.setUsername(getLoginUsername(httpServletRequest));
+        transferKeyRequest.setUsername(ShiroUtil.subjectUsername());
         TransferKeyResponse transferKeyResponse = getRepository().getKeyManager().transferKey(transferKeyRequest);
         Pem pem = PemUtils.fromTransferKeyResponse(transferKeyResponse.getKey(), transferKeyResponse.getDescriptor());
         return pem.toString();
     }
 
-    protected String getLoginUsername(HttpServletRequest httpServletRequest) {        
-        String header = httpServletRequest.getHeader(AUTHORIZATION_HEADER); // Authorization: Basic am9uYXRoYW46am9uYXRoYW4=
-        log.debug("Authorization: {}", header); // debug only, XXX TODO MUST DELETE
-        if (header == null || header.isEmpty()) {
-            return null;
-        }
-        String[] schemeCredential = header.split(" "); // Basic am9uYXRoYW46am9uYXRoYW4=
-        if (schemeCredential == null || schemeCredential.length < 2) {
-            log.debug("scheme credential: {}", (Object[]) schemeCredential);
-            return null;
-        }
-        if (!schemeCredential[0].equalsIgnoreCase(HttpServletRequest.BASIC_AUTH)) {
-            log.debug("scheme credential not {}", HttpServletRequest.BASIC_AUTH);
-            return null;
-        }
-        String basicUsernamePassword = new String(Base64.decodeBase64(schemeCredential[1]));
-        String[] usernamePassword = basicUsernamePassword.split(":");
-        if (usernamePassword == null || usernamePassword.length < 2) {
-            log.debug("username password invalid: {}", basicUsernamePassword);
-            return null;
-        }
-        
-        String username = usernamePassword[0];
-        String password = usernamePassword[1];
-        String host = httpServletRequest.getRemoteHost();
-        UsernamePasswordToken authenticationToken = new UsernamePasswordToken(username, password, false, host);
-        Subject subject = SecurityUtils.getSubject();
-        subject.login(authenticationToken);
-        if( subject.isAuthenticated() ) {
-            return username;
-        }
-        return null;
-    }
 
-    // does not work when anon filter is set in shiro.ini because then shiro doesn't process the authorization header and the subject is not authenticated
-    protected String getLoginUsernameFromSubject() {
-        Subject subject = SecurityUtils.getSubject();
-        if (subject.isAuthenticated()) {
-            PrincipalCollection principals = subject.getPrincipals(); // null when the subject is not authenticated
-            Collection<Username> usernames = principals.byType(Username.class);
-            Iterator<Username> it = usernames.iterator();
-            if (it.hasNext()) {
-                String username = it.next().getUsername();
-                log.debug("Got username: {}", username);
-                return username;
-            }
-        }
-        return null;
-    }
 }
